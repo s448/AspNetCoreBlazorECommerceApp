2402ff48f95ce5fa1ed0881d0dabba08a4350f00
s448 <elsaedmohassan@gmail.com>
2023-05-04 19:23:42 -0700

    admin dashboard d02 -- implement the product type adding

--- BlazorECommerce/Client/Services/ProductTypeService/IProductTypeService.cs
+++ BlazorECommerce/Client/Services/ProductTypeService/IProductTypeService.cs
@@ -5,6 +5,9 @@
         List<ProductType> ProductTypes { get; set; }
         event Action OnChange;
         Task GetProductTypes();
+        Task AddProductType(ProductType productType);
+        Task UpdateProductType(ProductType productType);
+        ProductType CreateNewProductType();
 
     }
 }
